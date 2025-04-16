4ec57f9d8e03b582555fe623e88f2f8735675a69
hungdungn47 <hungdungn47@gmail.com>
2025-04-16 15:39:39 +0700

    refactor: use react-hook-form to control form instead of useState

--- src/pages/Authentication/LoginPage.tsx
+++ src/pages/Authentication/LoginPage.tsx
@@ -1,19 +1,18 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginAPI } from "../../api";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type LoginInputs = {
+  email: string,
+  password: string,
+}
 
 export default function LoginPage() {
   const navigate = useNavigate()
-  const [formData, setFormData] = useState({ email: "", password: "" })
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value
-    })
-  }
-  const handleLogin = async () => {
-    await loginAPI(formData)
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginInputs>()
+  const onSubmit: SubmitHandler<LoginInputs> = async (data) => {
+    await loginAPI(data)
     navigate('/')
   }
   return (
@@ -25,19 +24,23 @@ export default function LoginPage() {
       margin: 'auto',
       height: '100vh'
     }}>
-      <Typography sx={{ textAlign: 'center', fontWeight: 'bold', marginBottom: "30px", color: "#1c81e6" }} variant="h4">Login</Typography>
-      <TextField name="email" label="Email" fullWidth size="medium" onChange={handleFormChange}></TextField>
-      <Box sx={{ height: 15 }}></Box>
-      <TextField name="password" label="Password" fullWidth size="medium" onChange={handleFormChange}></TextField>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginY: 2
-      }}>
-        <Typography>Don't have an account?</Typography>
-        <Typography onClick={() => navigate('/sign-up')} sx={{ fontWeight: 'bold', color: "#1c81e6", cursor: 'pointer' }}>Sign up</Typography>
-      </Box>
-      <Button onClick={handleLogin} sx={{ textTransform: 'none', paddingY: 1 }} variant="contained">Login</Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography sx={{ textAlign: 'center', fontWeight: 'bold', marginBottom: "30px", color: "#1c81e6" }} variant="h4">Login</Typography>
+        <TextField label="Email" fullWidth size="medium" {...register("email", { required: true })}></TextField>
+        {errors.email && <span style={{ color: 'red' }}>This field is required</span>}
+        <Box sx={{ height: 15 }}></Box>
+        <TextField label="Password" fullWidth size="medium" {...register("password", { required: true })}></TextField>
+        {errors.password && <span style={{ color: 'red' }}>This field is required</span>}
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginY: 2
+        }}>
+          <Typography>Don't have an account?</Typography>
+          <Typography onClick={() => navigate('/sign-up')} sx={{ fontWeight: 'bold', color: "#1c81e6", cursor: 'pointer' }}>Sign up</Typography>
+        </Box>
+        <Button fullWidth type="submit" sx={{ textTransform: 'none', paddingY: 1 }} variant="contained">Login</Button>
+      </form>
     </Box>
   )
 }
--- src/pages/Authentication/SignUpPage.tsx
+++ src/pages/Authentication/SignUpPage.tsx
@@ -1,21 +1,23 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { registerAPI } from "../../api";
-import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type RegisterInputs = {
+  email: string,
+  username: string,
+  password: string
+}
 
 export default function SignUpPage() {
   const navigate = useNavigate()
-  const [formData, setFormData] = useState({ username: "", email: "", password: "" })
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value
-    })
-  }
-  const handleRegister = async () => {
-    await registerAPI(formData)
+  const { register, handleSubmit, formState: { errors } } = useForm<RegisterInputs>()
+  const onSubmit: SubmitHandler<RegisterInputs> = async (data) => {
+    console.log(data);
+    await registerAPI(data)
     navigate('/login')
   }
+
   return (
     <Box sx={{
       width: '40%',
@@ -25,21 +27,26 @@ export default function SignUpPage() {
       margin: 'auto',
       height: '100vh'
     }}>
-      <Typography sx={{ textAlign: 'center', fontWeight: 'bold', marginBottom: "30px", color: "#1c81e6" }} variant="h4">Sign up</Typography>
-      <TextField onChange={handleFormChange} name="username" label="Username" fullWidth size="medium"></TextField>
-      <Box sx={{ height: 15 }}></Box>
-      <TextField onChange={handleFormChange} name="email" label="Email" fullWidth size="medium"></TextField>
-      <Box sx={{ height: 15 }}></Box>
-      <TextField onChange={handleFormChange} name="password" label="Password" fullWidth size="medium"></TextField>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginY: 2
-      }}>
-        <Typography>Already have an account?</Typography>
-        <Typography onClick={() => navigate('/login')} sx={{ fontWeight: 'bold', color: "#1c81e6", cursor: 'pointer' }}>Login</Typography>
-      </Box>
-      <Button onClick={handleRegister} sx={{ textTransform: 'none', padding: 1 }} variant="contained">Sign up</Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography sx={{ textAlign: 'center', fontWeight: 'bold', marginBottom: "30px", color: "#1c81e6" }} variant="h4">Sign up</Typography>
+        <TextField {...register("username", { required: true })} name="username" label="Username" fullWidth size="medium"></TextField>
+        {errors.username && <span style={{ color: 'red' }}>This field is required</span>}
+        <Box sx={{ height: 15 }}></Box>
+        <TextField {...register("email", { required: true })} name="email" label="Email" fullWidth size="medium"></TextField>
+        {errors.email && <span style={{ color: 'red' }}>This field is required</span>}
+        <Box sx={{ height: 15 }}></Box>
+        <TextField {...register("password", { required: true })} name="password" label="Password" fullWidth size="medium"></TextField>
+        {errors.password && <span style={{ color: 'red' }}>This field is required</span>}
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginY: 2
+        }}>
+          <Typography>Already have an account?</Typography>
+          <Typography onClick={() => navigate('/login')} sx={{ fontWeight: 'bold', color: "#1c81e6", cursor: 'pointer' }}>Login</Typography>
+        </Box>
+        <Button type="submit" sx={{ textTransform: 'none', padding: 1 }} variant="contained" fullWidth>Sign up</Button>
+      </form>
     </Box>
   )
 }
